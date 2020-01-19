fdfbd83ae8162d802e693ee92bbdb71342b15bb7
Кирилл Краснов <kirill.v.krasnov@gmail.com>
2020-01-19 21:00:12 +0300

    Урок 3 ДЗ

--- static_src/index.jsx
+++ static_src/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MessageField from './components/MessageField';
+import Layout from './components/Layout';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 ReactDOM.render(
    <MuiThemeProvider>
-      <MessageField />
+      <Layout />
    </MuiThemeProvider>,
    document.getElementById('root'),
 );
